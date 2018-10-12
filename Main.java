09d6a6f5ce20d85635db2eeb9419e1f682f563c5
Porchetta <clarantonio98@gmail.com>
2018-10-12 02:05:50 +0200

    [UPDATE] Corretta formattazione, migliorato un po' il codice

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.HttpURLConnection;
+
+import org.json.JSONObject;
+
 public class Main {
-
-
-    private static final String API_KEY = "e8024a81ac61e929b25e57016a5bbe14";
-   private static final String API_URL_BASE = "http://food2fork.com/api/search?key=" + API_KEY + "&q=";
-
+	private static final String API_KEY = "e8024a81ac61e929b25e57016a5bbe14";
+	private static final String API_URL_BASE = "http://food2fork.com/api/search?key=" + API_KEY + "&q=";
 
     /**
      * Performs an HTTP GET and parses the response body as JSON.
@@ -45,56 +45,34 @@ public class Main {
         return run(url);
     }*/
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+	    URL url = new URL(API_URL_BASE + URLEncoder.encode("polLo", "UTF-8") + "&page=2");
 
+	    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
 
-      try{
+		int status = con.getResponseCode();
+		if (status == HttpURLConnection.HTTP_MOVED_TEMP || status == HttpURLConnection.HTTP_MOVED_PERM) {
+			String location = con.getHeaderField("Location");
+			URL newUrl = new URL(location);
 
-      URL url = new URL(API_URL_BASE + URLEncoder.encode("polLo", "UTF-8")+"&page=2");
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
+			con = (HttpURLConnection) newUrl.openConnection();
+			con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
+	    }
 
-      int status = con.getResponseCode();
-      if (status == HttpURLConnection.HTTP_MOVED_TEMP
-      || status == HttpURLConnection.HTTP_MOVED_PERM) {
-        String location = con.getHeaderField("Location");
-        URL newUrl = new URL(location);
-        System.out.println(newUrl);
-        con = (HttpURLConnection) newUrl.openConnection();
-        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
-      }
-      System.out.println(con.getResponseCode());
+	    System.out.println("Qui laggo");
+	    System.out.println("Guarda quanto ci ho messo! " + con.getInputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-      BufferedReader in = new BufferedReader(
-      new InputStreamReader(con.getInputStream()));
-
-      String inputLine;
-      StringBuffer content = new StringBuffer();
-      while ((inputLine = in.readLine()) != null) {
-          content.append(inputLine);
-      }
-      in.close();
-      JSONObject ricettine = new JSONObject (content.toString());
-      System.out.println(ricettine.toString());
-      
-
-
-    }
-    catch(IOException e) {
-        e.printStackTrace();
-    }
-
-
-
-
-        /*try {
-            final JSONObject searchResults = search("shredded chicken");
-            // Get and print the first recipe.
-            final JSONObject recipe = getRecipe(getRecipeIds(searchResults).get(0));
-            System.out.println(recipe.toString(2));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-}
-
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+		in.close();
+		
+		JSONObject ricettine = new JSONObject (content.toString());
+		System.out.println(ricettine.toString());
+	}
 }
